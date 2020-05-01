7a3da934aea697a9ce4f5d5d68df1224a651bbdd
geroboham <geroal.xander@gmail.com>
2020-05-01 15:08:35 +0200

    home page to-do

--- Cliente/src/Pages/HomePage/HomePage.js
+++ Cliente/src/Pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './homePage.css';
 import AuthService from '../../Services/AuthService';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 
@@ -9,17 +9,24 @@ export default function Home() {
     const avatar = 'http://i.stack.imgur.com/Dj7eP.jpg';
 
     return (
-        <div className="d-block">
-            <nav className="navbar sticky-top navbar-dark bg-dark">
-                <Link className="navbar-brand" to={"/profile/"+User._id}>
-                    <img id="avatar" className="d-inline-block align-top" alt="" style={{backgroundImage: `url(${avatar})`}}/>
-                    {User.name}
-                </Link>
-                <form className="form-inline">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </nav>
+        <div className="container-fluid">
+            <div className="row">
+                <nav class="col-12 navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+                    <div className="col-2">
+                        <Link className="navbar-brand ml-lg-3" to={"/profile/" + User._id}>
+                            <img id="avatar" alt="" style={{ backgroundImage: `url(${avatar})` }} />
+                            {User.name}
+                        </Link>
+                    </div>
+                    <div className="col-7"></div>
+                    <div className="col-3">
+                        <form className="float-right form-inline col-12">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
         </div>
     )
 }
