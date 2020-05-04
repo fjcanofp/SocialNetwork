bb985819d3b43536972500e8347356ce750f5e94
dream <dreamjose@gmail.com>
2020-05-04 12:36:46 +0200

    Color Reactions

--- Cliente/src/Components/ReactionBar/ReactionBar.js
+++ Cliente/src/Components/ReactionBar/ReactionBar.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import './ReactionBar.css';
 
 export default function ReactionBar() {
+
+    const handleClick = (evt) =>{
+        if(evt.target.className === "far fa-thumbs-up" || evt.target.className ==="far fa-thumbs-up darkorange") evt.target.classList.toggle('darkorange');
+        else if(evt.target.className === "far fa-thumbs-down" ||  evt.target.className === "far fa-thumbs-down blue") evt.target.classList.toggle('blue');
+        else if(evt.target.className === "far fa-star" || evt.target.className === "far fa-star yellow") evt.target.classList.toggle('yellow');
+        else if(evt.target.className === "far fa-heart" ||  evt.target.className === "far fa-heart red") evt.target.classList.toggle('red');
+    }
 
     return (
         <div className="row">
@@ -8,10 +16,10 @@ export default function ReactionBar() {
                 <div type="button" className="btn" data-toggle="collapse" data-target="#collapseExample">
                     comments
                 </div>
-                <button className="btn"><i className="far fa-thumbs-up"></i></button>
-                <button className="btn"><i className="far fa-thumbs-down"></i></button>
-                <button className="btn"><i className="far fa-star"></i></button>
-                <button className="btn"><i className="far fa-heart"></i></button>
+                <button onClick={handleClick} className="btn"><i className="far fa-thumbs-up"/></button>
+                <button onClick={handleClick} className="btn"><i className="far fa-thumbs-down"/></button>
+                <button onClick={handleClick} className="btn"><i className="far fa-star"/></button>
+                <button onClick={handleClick} className="btn"><i className="far fa-heart"/></button>
             </div>
         </div>
     )
